dff07b3e52cf3080f6731a299388678df52dd48b
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-24 00:24:49 +0200

    Update blog.tsx jk

--- components/homepage/blog.tsx
+++ components/homepage/blog.tsx
@@ -1,7 +1,7 @@
 "use client";
 // pages/index.js
 import { Button } from "@/components/ui/button";
-import *, { useState, useEffect, useRef } from "react";
+import  { React, useState, useEffect, useRef } from "react";
 import Link from 'next/link';
 import Autoplay from "embla-carousel-autoplay";
 import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
