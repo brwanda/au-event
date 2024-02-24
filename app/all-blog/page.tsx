707f1027e896d5a9fe455dd4322c1a7cf5be8ac1
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-24 12:30:26 +0200

    mumit

--- app/all-blog/page.tsx
+++ app/all-blog/page.tsx
@@ -48,17 +48,16 @@ export default function Component() {
             </div>
             <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
               <div className="flex-shrink-0">
-                <img
-                  alt=""
-                  className="h-48 w-full object-cover"
-                  height="192"
-                  src="/placeholder.svg"
-                  style={{
-                    aspectRatio: "384/192",
-                    objectFit: "cover",
-                  }}
-                  width="384"
-                />
+              <svg
+          className="h-48 w-full text-gray-200 dark:text-gray-600"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 18"
+          width="384"
+        >
+          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+        </svg>
               </div>
               <div className="flex-1 bg-white p-6 flex flex-col justify-between">
                 <div className="flex-1">
@@ -83,17 +82,16 @@ export default function Component() {
             </div>
             <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
               <div className="flex-shrink-0">
-                <img
-                  alt=""
-                  className="h-48 w-full object-cover"
-                  height="192"
-                  src="/placeholder.svg"
-                  style={{
-                    aspectRatio: "384/192",
-                    objectFit: "cover",
-                  }}
-                  width="384"
-                />
+              <svg
+          className="h-48 w-full text-gray-200 dark:text-gray-600"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 18"
+          width="384"
+        >
+          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+        </svg>
               </div>
               <div className="flex-1 bg-white p-6 flex flex-col justify-between">
                 <div className="flex-1">
