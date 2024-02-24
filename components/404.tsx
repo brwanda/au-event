434d77a182540f41a5da833ba93274fe4445c14c
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-25 00:06:25 +0200

    Update 404.tsx link

--- components/404.tsx
+++ components/404.tsx
@@ -87,7 +87,7 @@ export default function Home() {
        Stay tuned the page you wanted currently under development . please you can move to the other page
         </p>
         <div className="mt-8 flex space-x-4">
-          <Button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded roundnow">ABOUT US</Button>
+          <Link className="bg-blue-500 hover:bg-blue-600 text-white font-bold  rounded roundnow">ABOUT US</Link>
           <Button className="bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded roundnow">
             SEE GALLERY
           </Button>
