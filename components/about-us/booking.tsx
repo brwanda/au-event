05e869a0529603f5b073c8db147f23925260537c
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-24 03:56:19 +0200

    Update booking.tsx auto

--- components/about-us/booking.tsx
+++ components/about-us/booking.tsx
@@ -9,6 +9,7 @@ export default function Component() {
         height:"60vh auto",
       }}
     >
+      
       <div className="container mx-auto px-6 py-20 h-1/3 items-center">
         <div className="container w-full md:w-1/2 px-4 ">
           <h2 className="text-6xl font-bold-lg mb-2">Book Your Next Event With Us!</h2>
