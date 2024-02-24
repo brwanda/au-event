ed101886e8c6e1ad406d17e2b05f195e50258e73
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-25 00:19:56 +0200

    Update 404.tsx working

--- components/404.tsx
+++ components/404.tsx
@@ -88,9 +88,10 @@ export default function Home() {
         </p>
         <div className="mt-8 flex space-x-4">
           <Link href="/about-us" className="items-center text-center"><Button  className="bg-blue-500 hover:bg-blue-600 text-white font-bold  py-2  px-4 rounded roundnow">ABOUT US</Button></Link>
-          <Button className="bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded roundnow">
+           <Link href="/gallery" className="items-center text-center"> <Button className="bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded roundnow">
             SEE GALLERY
           </Button>
+             </Link>
         </div>
       </div>
     </div>
