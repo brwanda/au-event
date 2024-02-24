50ffed40a0cdf3280ee882847bbbed25a5840a3e
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-24 02:09:24 +0200

    Update newdata.js image back

--- newdata.js
+++ newdata.js
@@ -1,19 +1,3 @@
 export const MyImgUrl = [
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470205/Authentic%20Events/53437667486_0247ede963_o_imqw1d.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470200/Authentic%20Events/53437981909_5fd66c78a0_o_uyt9m1.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470199/Authentic%20Events/53493475930_8f17b7f5c5_o_ss1gbg.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470199/Authentic%20Events/53460156977_16cb8dd6a3_o_o9zdeb.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470199/Authentic%20Events/53494885461_85c415cba4_o_crc4jk.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470195/Authentic%20Events/53437994094_102e8b0057_o_djshos.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470192/Authentic%20Events/53436739222_72f0fd4dc2_o_p4g6dt.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470192/Authentic%20Events/53437994064_96b0fbc78d_o_qvss2a.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470191/Authentic%20Events/53437803923_8a2dcaa1d9_o_zfrmtv.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470186/Authentic%20Events/53437811988_636b9cd8e5_o_lii19w.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470185/Authentic%20Events/53436744112_db00235bb7_o_spameq.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470184/Authentic%20Events/53437806363_e9be215675_o_oifb74.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470164/Authentic%20Events/53160436675_b5621bf0fc_o_xveruq.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470162/Authentic%20Events/53126114808_4cd6c7c269_o_juebed.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470162/Authentic%20Events/53126114808_4cd6c7c269_o_juebed.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470155/Authentic%20Events/53126033195_31d73747ac_o_uy9p3r.jpg',
-  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470130/Authentic%20Events/53085162356_6369f35580_o_yi2jwc.jpg'
+  'https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470205/Authentic%20Events/53437667486_0247ede963_o_imqw1d.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470200/Authentic%20Events/53437981909_5fd66c78a0_o_uyt9m1.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470199/Authentic%20Events/53493475930_8f17b7f5c5_o_ss1gbg.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470199/Authentic%20Events/53460156977_16cb8dd6a3_o_o9zdeb.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470199/Authentic%20Events/53494885461_85c415cba4_o_crc4jk.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470195/Authentic%20Events/53437994094_102e8b0057_o_djshos.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470192/Authentic%20Events/53436739222_72f0fd4dc2_o_p4g6dt.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470192/Authentic%20Events/53437994064_96b0fbc78d_o_qvss2a.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470191/Authentic%20Events/53437803923_8a2dcaa1d9_o_zfrmtv.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470186/Authentic%20Events/53437811988_636b9cd8e5_o_lii19w.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470185/Authentic%20Events/53436744112_db00235bb7_o_spameq.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470184/Authentic%20Events/53437806363_e9be215675_o_oifb74.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470164/Authentic%20Events/53160436675_b5621bf0fc_o_xveruq.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470162/Authentic%20Events/53126114808_4cd6c7c269_o_juebed.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470162/Authentic%20Events/53126114808_4cd6c7c269_o_juebed.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470155/Authentic%20Events/53126033195_31d73747ac_o_uy9p3r.jpg','https://res.cloudinary.com/dxtjjbk95/image/upload/v1708470130/Authentic%20Events/53085162356_6369f35580_o_yi2jwc.jpg'
   ]
