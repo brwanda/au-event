42eadfcfa6f4d0c22a96d028a0156d8baf82932f
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-24 12:37:46 +0200

    ssasa

--- app/all-blog/page.tsx
+++ app/all-blog/page.tsx
@@ -1,6 +1,6 @@
 import Blogpage from "@/components/blog/blogpage"
 import Header from "@/components/header"
-import Footer from "@/components/Footer"
+import Footer from "@/components/footer"
 
 export default function Component() {
   return (
