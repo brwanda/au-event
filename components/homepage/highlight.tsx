5577cf0a045c28442be46b0f8ffc6e33b0629d79
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-25 11:44:29 +0200

    ArrowsIcon

--- components/homepage/highlight.tsx
+++ components/homepage/highlight.tsx
@@ -2,7 +2,7 @@
 import React, { useRef, useState, useEffect, createRef } from 'react';
 import './home.css';
 import { useRouter } from 'next/navigation';
-import { ArrowLeftIcon, PlayIcon, PauseIcon, ArrowsExpandIcon, ArrowsContractIcon } from '@heroicons/react/24/solid';
+import { ArrowLeftIcon, PlayIcon, PauseIcon, ArrowsIcon } from '@heroicons/react/24/solid';
 
 function VideoPlayerNow() {
   const videoRef = useRef(null);
@@ -122,7 +122,7 @@ const togglePlayPause = () => {
                       <ArrowRightIcon className="h-6 w-6" />
                       </button>
                       <button className='fullscreen' onClick={handleToggleFullScreen}>
-                      {isFullScreen ? <ArrowsContractIcon className="h-6 w-6" /> : <ArrowsExpandIcon className="h-6 w-6" />}
+                      {isFullScreen ? <ArrowsIcon className="h-6 w-6" /> : <ArrowsIcon className="h-6 w-6" />}
                       </button>
                     </li>
                     <li className='options right'>
