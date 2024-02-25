4d7d84b9b709f590fb1f0aa30e2960cf6f459e96
brwanda <108659906+brwanda@users.noreply.github.com>
2024-02-25 19:48:19 +0200

    sas

--- components/homepage/ourparterners.tsx
+++ components/homepage/ourparterners.tsx
@@ -16,9 +16,8 @@ export default function Component() {
               </div>
             </div>
           </div>
-          <Carousel>
-          <CarouselContent className="mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-          
+          <Carousel  className="mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          <CarouselContent>
             
         
           {
